6f69d4cdea258b7cffc8496c7e93a468b93008b3
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-09-13 17:46:31 +0900

    인터페이스

---
+++ interface/interface/interface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Interface
+{
+    public interface ICarStandard   //구현을 강제함 추상클래스랑 같음 ,  차이점-  추상클래스는 자체 함수구현도 가능하다 . 
+    {
+        void Left();
+    }
+    abstract class KS
+    { 
+        public abstract void Back();
+    }
+    class MyCar : KS, ICarStandard
+    {
+        public void Left() => Console.WriteLine("좌회전");
+        public override void Back() => Console.WriteLine("후진");
+    }
+
+    class Interface
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+}
