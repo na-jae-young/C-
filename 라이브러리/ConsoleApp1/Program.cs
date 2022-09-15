fe25fcbad85b9b0f945f79e2c91d85a43907c3cb
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-09-15 18:10:05 +0900

    라이브러리   생성   사용

---
+++ 라이브러리/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JyClassLibrary1.Algorithm.sort(); // 라이브러리  프로잭트 참조 추가 하여 사용 할수 있게 되었다 . dll 파일이 복사되었으므로 
+
+
+        }
+    }
+}
+// 종속성 -> 프로잭트 참조 추가  -> dll 파일 복사됨 
---
+++ 라이브러리/jyClassLibrary1/Algorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JyClassLibrary1
+{
+    public class Algorithm
+    {
+
+        public static void sort() => Console.WriteLine("OK");
+    }
+}
+// dll 파일 생성됨 
